014f2275c86815ee538a70e8744bd7d6f827c678
Zach Karst <zach@karst.co>
2014-06-12 23:46:20 -0400

    Add pending tests for user adding artists to favorites

---
+++ giggidy/spec/features/favorites_add_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+feature "Add artists to favorites" do 
+
+  scenario "artist snippet contains name"
+
+  scenario "artist snippet contains link to ..."
+
+  scenario "artist snippet shows upcoming shows"
+
+  scenario "user adds a favorite artist"
+
+end
