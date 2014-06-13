83307643fa0bf62f4bb2b2052fd9132afb3eba08
Alexander Pan <alexanderdavidpan@gmail.com>
2014-06-13 11:55:59 -0400

    Add get artist name from search_box function

--- giggidy/app/assets/javascripts/search_box.js
+++ giggidy/app/assets/javascripts/search_box.js
@@ -1,3 +1,9 @@
-$(function() {
+function searchSuggest() {
   $("#search_box").suggest({filter:'(all type:/music/artist)'});
-});
+};
+
+function getArtistName() {
+  artist = $("#search_box").val();
+  return artist;
+}
+
