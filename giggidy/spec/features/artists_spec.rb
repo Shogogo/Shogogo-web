efdd03517cbdd6e1614fb2f7c21180967cab4d44
Zach Karst <zach@karst.co>
2014-06-12 21:49:56 -0400

    Remove module defining search box in artists feature test

--- giggidy/spec/features/artists_spec.rb
+++ giggidy/spec/features/artists_spec.rb
@@ -1,18 +1,10 @@
-require 'spec-helper'
-
-module MenuSteps
-  def search_box
-    find("search_box")
-  end
-end
-
+require 'spec_helper'
 
 feature "Artist search" do 
   scenario "user can see live search results", js: true do
     visit root_path
 
-    search_box.fill_in "artist-search", with: "Fleetwood"
-    expect(page).to have_content ;
-
-
+    find("#search_box").fill_in "artist-search", with: "Fleetwood"
+    expect(page).to have_content("Fleetwood Mac")
+  end
 end
