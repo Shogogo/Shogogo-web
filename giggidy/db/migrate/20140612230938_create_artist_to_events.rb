79b17c595f5ba6e3b78b0c698b4dbb0ba6ad951e
justinphelps <justin.phelps@gmail.com>
2014-06-13 17:48:56 -0400

    remove artist/venue/event functionality from models/migrations. We will rely on seatgeek API for that instead. Update schema docs

--- giggidy/db/migrate/20140612230938_create_artist_to_events.rb
+++
@@ -1,8 +0,0 @@
-class CreateArtistToEvents < ActiveRecord::Migration
-  def change
-    create_table :artist_to_events do |t|
-      t.belongs_to :artist, index: true
-      t.belongs_to :event, index: true
-    end
-  end
-end
--- giggidy/db/schema.rb
+++ giggidy/db/schema.rb
@@ -11,48 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140612230938) do
+ActiveRecord::Schema.define(version: 20140612230818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "artist_to_events", force: true do |t|
-    t.integer "artist_id"
-    t.integer "event_id"
-  end
-
-  add_index "artist_to_events", ["artist_id"], name: "index_artist_to_events_on_artist_id", using: :btree
-  add_index "artist_to_events", ["event_id"], name: "index_artist_to_events_on_event_id", using: :btree
-
-  create_table "artists", force: true do |t|
-    t.string  "name"
-    t.string  "image_url"
-    t.integer "seatgeek_id"
-    t.string  "type"
-  end
-
-  create_table "events", force: true do |t|
-    t.string   "title"
-    t.string   "url"
-    t.datetime "datetime_local"
-    t.integer  "venue_id"
-  end
-
-  create_table "user_to_events", force: true do |t|
+  create_table "interests", force: true do |t|
     t.integer "user_id"
-    t.integer "event_id"
+    t.integer "geekseat_artist_id"
   end
 
-  add_index "user_to_events", ["event_id"], name: "index_user_to_events_on_event_id", using: :btree
-  add_index "user_to_events", ["user_id"], name: "index_user_to_events_on_user_id", using: :btree
-
-  create_table "user_to_interests", force: true do |t|
-    t.integer "user_id"
-    t.integer "event_id"
-  end
-
-  add_index "user_to_interests", ["event_id"], name: "index_user_to_interests_on_event_id", using: :btree
-  add_index "user_to_interests", ["user_id"], name: "index_user_to_interests_on_user_id", using: :btree
+  add_index "interests", ["user_id"], name: "index_interests_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "username"
@@ -67,17 +36,6 @@ ActiveRecord::Schema.define(version: 20140612230938) do
     t.boolean  "wants_text"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "venues", force: true do |t|
-    t.string  "name"
-    t.string  "address"
-    t.string  "city"
-    t.string  "url"
-    t.string  "state"
-    t.integer "postal_code"
-    t.decimal "longitude"
-    t.decimal "latitude"
   end
 
 end
