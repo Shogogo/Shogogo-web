c27f1a060ee2e585256a16d0af154ab6ec8b146e
Zach Karst <zach@karst.co>
2014-06-14 12:57:17 -0400

    Add additional nodes to FavoritesPresenter
    - bandImageNode will display small image of band in bandContainerNode
    - set bandImageNode to img variable containing img create element
    
    :

--- giggidy/app/assets/javascripts/favorites_presenter.js
+++ giggidy/app/assets/javascripts/favorites_presenter.js
@@ -1,22 +1,28 @@
 FavoritesPresenter = function(band) {
-  this.band = band;
-  var div = document.createElement('div');
-  var button = document.createElement('button');
+    this.band = band;
+    var div = document.createElement('div');
+    var button = document.createElement('button');
+    var img = document.createElement('img');
 };
 
 FavoritesPresenter.prototype = {
-  present: function() {
-    var favoritesMenuNode = div;
-    var bandNode = div;
-    var artistNameNode = div;
-    var removeArtistNode = button;
-    var saveNode = button;
+    present: function() {
+        var favoritesMenuNode = div;
+        var bandContainerNode = div;
+        var bandNameNode = div;
+        var bandImageNode = img;
+        var removeArtistNode = button;
+        var saveNode = button;
 
-    favoritesMenuNode.className = "favorites_menu";
+        favoritesMenuNode.className = "favorites_menu";
+
+        bandNode.className = "favorites_band_item";
+
+
 
 
   }
 
 
 
-};
+}
