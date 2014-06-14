c0486eafd67e9e285eff4020cb09f9c78443b6e6
Alexander Pan <alexanderdavidpan@gmail.com>
2014-06-14 13:54:06 -0400

    Instantiate Twilio client using id and token from ENV

--- giggidy/app/controllers/sms/send_text_controller.rb
+++ giggidy/app/controllers/sms/send_text_controller.rb
@@ -3,6 +3,9 @@ class SendTextController < ApplicationController
   end
 
   def send_sms
+    twilio_sid = ENV['twilio_account_sid']
+    twilio_token = ENV['twilio_auth_token']
+ 
     @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
  
     @twilio_client.account.sms.messages.create(
