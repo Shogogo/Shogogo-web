8a2a6fcc7b169eb8427a8e90a6958df31ccb11e2
Zach Karst <zach@karst.co>
2014-06-14 18:58:01 -0400

    Add instance of FavoritesPresenter with argument favoriteList
    FavoritesView now replaces the existing favorites menu div with one created using the FavoritesPresenter.

--- giggidy/app/assets/javascripts/favorites_view.js
+++ giggidy/app/assets/javascripts/favorites_view.js
@@ -1,10 +1,11 @@
 FavoritesView = function() {
     this.favoritesMenuNode = document.getElementsByClassName('favorites_menu')[0];
+    this.favoritesPresenter = new FavoritesPresenter();
 };
 
 FavoritesView.prototype = {
-    draw:function(band) {
-        var newFavoritesMenu = new FavoritesPresenter(band).present();
+    draw:function(favoriteList) {
+        var newFavoritesMenu = this.favoritesPresenter.present(favoriteList);
         this.favoritesMenuNode.innerHTML = newFavoritesMenu.innerHTML;
     }
 };
