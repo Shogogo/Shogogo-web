2f34048e20d9e8c3a13138072a9e3b8dfe92c1cd
Zach Karst <zach@karst.co>
2014-06-13 20:19:15 -0400

    Refactor ajax request to seatgeek api in application_controller.js
    Replace .ajax with .getJSON

--- giggidy/app/assets/javascripts/application_controller.js
+++ giggidy/app/assets/javascripts/application_controller.js
@@ -1,20 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function() {
     searchBox = new SearchBox();
     searchBox.searchSuggest();
     $(document).keypress(function(e) {
         if(e.which == 13) {
             var artistName = searchBox.getArtistName();
             var preparedArtistName = searchBox.preparedAristQuery(artistName);
-            $.ajax( {
-            type: 'GET',
-            url: preparedArtistName,
-            dataType: 'json'
-        }).done(function(artistInfo){
-            var artistData = artistInfo.performers[0];
-            var artist = searchBox.parseArtistInfo(artistData);
-            return artist;
-        });
-    }
-  });
+            $.getJSON(preparedArtistName).done(function(artistInfo) {
+                var artistData = artistInfo.performers[0];
+                var artist = searchBox.parseArtistInfo(artistData);
+                return artist;
+            });
+        }
+    });
 });
 
