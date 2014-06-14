a6ea4df2eb56a01f7acebfbbe96b2bd918f30619
Zach Karst <zach@karst.co>
2014-06-14 18:53:19 -0400

    Edit FavoritesPresenter to parse favorites list array
    FavoritesPresenter now takes an array argument and loops through each band object in the aray. For each object, band container, name, image, and remove nodes are created and appended to favoritesMenuNode.
    
    FavoritesPresenter previously took a band object as an argument. This resulted in appending only one band object to the favorites list and the favorites list only existing as a div in the dom, not an object.

--- giggidy/app/assets/javascripts/favorites_presenter.js
+++ giggidy/app/assets/javascripts/favorites_presenter.js
@@ -1,37 +1,41 @@
-FavoritesPresenter = function(band) {
-    this.band = band;
+FavoritesPresenter = function() {
 };
 
 FavoritesPresenter.prototype = {
     present: function(favoriteList) {
         var favoritesMenuNode = document.createElement('div');
-        var bandContainerNode = document.createElement('div');
-        var bandNameNode = document.createElement('div');
-        var bandImageNode = document.createElement('img');
-        var removeBandNode = document.createElement('button');
         var saveNode = document.createElement('button');
 
         favoritesMenuNode.className = "favorites_menu";
-        bandContainerNode.className = "favorites_band_item";
-        bandNameNode.className = "favorites_band_name";
-        bandImageNode.className = "favorites_band_image";
-        removeBandNode.className = "favorites_band_remove";
+        saveNode.className = "favorites_save";
+            // Button will add favorites to notify list and request user phone - change text to something more descriptive.
+        saveNode.innerText = "Notify Me!";
         
-        favoriteList.forEach(function(band) {
-            bandNameNode.innerText = band.name;
-            bandImageNode.src = band.image_url_small;
+        for (i = 0; i < favoriteList.length; i++) {
+            var bandContainerNode = document.createElement('div');
+            var bandNameNode = document.createElement('div');
+            var bandImageNode = document.createElement('img');
+            var removeBandNode = document.createElement('button');
+
+            bandContainerNode.className = "favorites_band_item";
+
+            bandNameNode.className = "favorites_band_name";
+            bandNameNode.innerText = favoriteList[i].name;
+            
+            bandImageNode.className = "favorites_band_image";
+            bandImageNode.src = favoriteList[i].image_url_small;
+            
+            removeBandNode.className = "favorites_band_remove";
             removeBandNode.innerText = "RM";
+
             bandContainerNode.appendChild(bandImageNode);
             bandContainerNode.appendChild(bandNameNode);
             bandContainerNode.appendChild(removeBandNode);
                 // Testing only - remove text - use img of negative sign
                 // removeBandNode.src = "button image url";
             favoritesMenuNode.appendChild(bandContainerNode);
-        });
-
-        saveNode.className = "favorites_save";
-            // Button will add favorites to notify list and request user phone - change text to something more descriptive.
-        saveNode.innerText = "Notify Me!";
+        }
+        
         favoritesMenuNode.appendChild(saveNode);
 
         return favoritesMenuNode;
