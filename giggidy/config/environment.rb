b2a6e37755c3deb084803b8cc307b715f9713760
Alexander Pan <alexanderdavidpan@gmail.com>
2014-06-14 13:51:31 -0400

    Remove Twilio Client from environment

--- giggidy/config/environment.rb
+++ giggidy/config/environment.rb
@@ -1,11 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-#Twilio client.
-CLIENT = Twilio::REST::Client.new do |config|
-  config.twilio_account_sid = ENV['twilio_account_sid']
-  config.twilio_auth_token = ENV['twilio_auth_token']
-end
-
 # Initialize the Rails application.
 Rails.application.initialize!
