9b4d294b6d5b3e03586f92732148d3466de8939f
Zach Karst <zach@karst.co>
2014-06-14 16:54:19 -0400

    Add FavoriteList add band and remove band methods

--- giggidy/app/assets/javascripts/favorites_list.js
+++ giggidy/app/assets/javascripts/favorites_list.js
@@ -4,9 +4,12 @@ FavoriteList = function() {
 
 FavoriteList.prototype = {
     addBand: function(band) {
-
+        this.list.push(band);
     },
-    removeBand: function(band) {
-
+    removeBand: function(band_literal) {
+        this.list.forEach(function(band) {
+            if (list[band_literal] === band)
+                list.splice(band_literal, 1);
+        });
     }
 };
