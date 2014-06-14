9dcf796af240e004125826ec9ba75e758c984660
Zach Karst <zach@karst.co>
2014-06-13 22:19:27 -0400

    Add BandView to render band container
    - New instance of BandView
    - Pass parsed artist object from api to BandView

--- giggidy/app/assets/javascripts/application_controller.js
+++ giggidy/app/assets/javascripts/application_controller.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
+    var bandView = new BandView();
+
     searchBox = new SearchBox();
     searchBox.searchSuggest();
+    
     $(document).keypress(function(e) {
         if(e.which == 13) {
             var artistName = searchBox.getArtistName();
             var preparedArtistName = searchBox.preparedAristQuery(artistName);
+            
             $.getJSON(preparedArtistName).done(function(artistInfo) {
                 var artistData = artistInfo.performers[0];
                 var artist = searchBox.parseArtistInfo(artistData);
-                return artist;
+                bandView.draw(artist);
             });
         }
     });
