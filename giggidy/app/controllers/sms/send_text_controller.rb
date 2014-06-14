db64570e6a2c5b2b786253abdd82973c6ea41823
Alexander Pan <alexanderdavidpan@gmail.com>
2014-06-14 13:23:44 -0400

    Add send_sms method to SendTextController with stubbed info

--- giggidy/app/controllers/sms/send_text_controller.rb
+++ giggidy/app/controllers/sms/send_text_controller.rb
@@ -1,4 +1,12 @@
 class SendTextController < ApplicationController
   def index
   end
+
+  def send_sms
+    CLIENT.account.messages.create(
+      :from => 'our_phone_number',
+      :to => 'user_phone_number',
+      :body => 'Your favorite band is in town!'
+    )
+  end
 end
