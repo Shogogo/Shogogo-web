e1ab0f40b0215dcaf2cf52dcf268186ad26106e4
Zach Karst <zach@karst.co>
2014-06-13 20:05:15 -0400

    Remove whitespace from application_controller.js

--- giggidy/app/assets/javascripts/application_controller.js
+++ giggidy/app/assets/javascripts/application_controller.js
@@ -1,21 +1,20 @@
 $(document).ready(function(){
-  searchBox = new SearchBox();
-  searchBox.searchSuggest();
-  $(document).keypress(function(e) {
-    if(e.which == 13) {
-      var artistName = searchBox.getArtistName();
-      var preparedArtistName = searchBox.preparedAristQuery(artistName);
-      
-        $.ajax({
+    searchBox = new SearchBox();
+    searchBox.searchSuggest();
+    $(document).keypress(function(e) {
+        if(e.which == 13) {
+            var artistName = searchBox.getArtistName();
+            var preparedArtistName = searchBox.preparedAristQuery(artistName);
+            $.ajax( {
             type: 'GET',
             url: preparedArtistName,
             dataType: 'json'
         }).done(function(artistInfo){
             var artistData = artistInfo.performers[0];
             var artist = searchBox.parseArtistInfo(artistData);
-            return artist
+            return artist;
         });
     }
-  })
+  });
 });
 
