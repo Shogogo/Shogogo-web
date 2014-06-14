191a487eea1979a5a1db50bac8be361ff69c27f9
Zach Karst <zach@karst.co>
2014-06-14 12:14:32 -0400

    Add nodes to be appended to favorites menu div by FavoritesPresenter
    Nodes:
    - favoritesMenuNode
    - bandNode
    - saveNode

--- giggidy/app/assets/javascripts/favorites_presenter.js
+++ giggidy/app/assets/javascripts/favorites_presenter.js
@@ -1,13 +1,17 @@
 FavoritesPresenter = function(band) {
-  this.band = band
+  this.band = band;
+  var div = document.createElement('div');
 };
 
 FavoritesPresenter.prototype = {
   present: function() {
-    
+    var favoritesMenuNode = div;
+    var bandNode = div;
+    var saveNode = document.createElement('button');
 
 
-  };
+
+  }
 
 
 
