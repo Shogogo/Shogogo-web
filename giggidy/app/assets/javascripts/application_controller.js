199cadda84b1ea0872e1370612eb7f32e837e4a2
Ryan Rebo & Zach Karst <pair+reboryan.zkarst@devbootcamp.com>
2014-06-15 16:37:48 -0400

    Add frontpage styling and HTML
    -freebase styling
    -searchbox styling
    -band container styling

--- giggidy/app/assets/javascripts/application_controller.js
+++ giggidy/app/assets/javascripts/application_controller.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 
     searchBox = new SearchBox();
     // searchBox.searchSuggest();
- 
+
     $("#search_box")
         .suggest({filter:'(all type:/music/artist)'
                 , flyout: false
@@ -17,18 +17,18 @@ $(document).ready(function() {
         .bind('fb-select', function(e) {
             var artistName = searchBox.getArtistName();
             var preparedArtistName = searchBox.preparedAristQuery(artistName);
-            
+
             $.getJSON(preparedArtistName).done(function(artistInfo) {
                 var artistData = artistInfo.performers[0];
                 artist = searchBox.parseArtistInfo(artistData);
                 bandView.draw(artist);
             });
     });
-    
+
     $('#band_container').on('click', 'button', function(e) {
         e.preventDefault();
         favoriteList.addBand(artist);
         favoritesView.draw(favoriteList.list);
-        $('#band_container').empty();
+        $('#band_container').empty().hide();
     });
 });
