66067abac9e0849e75347a058187b1cc84daf104
Alexander Pan <alexanderdavidpan@gmail.com>
2014-06-14 20:04:04 -0400

    Add visitor model psuedocode for future Redis implementation

--- giggidy/app/models/visitor.rb
+++ giggidy/app/models/visitor.rb
@@ -1,3 +1,18 @@
 class Visitor
+  def self.is_new?
+    #if session[visitor_id] = nil
+    # => return true
+    #else
+    # => return false
+  end
+
+  def self.assign_visitor_id
+    #if self.is_new? is true 
+    # => set unique redis visitor_id in session
+  end
+
+  def self.add_favorite
+    #add/update redis visitor_id: value
+  end
 
 end
