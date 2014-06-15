f2acd2e8617643f1311f6d520947c3170935d3f9
Ryan Rebo & Zach Karst <pair+reboryan.zkarst@devbootcamp.com>
2014-06-15 18:23:35 -0400

    Add animated slide in menu -add Jquery to band_presenter.js -add CSS3 transitions to animate faves menu

--- giggidy/app/assets/javascripts/band_presenter.js
+++ giggidy/app/assets/javascripts/band_presenter.js
@@ -9,15 +9,15 @@ BandPresenter.prototype = {
         var nameNode = document.createElement('div');
         var tourNode = document.createElement('div');
         var addNode = document.createElement('button');
-       
+
         bandPanelNode.id = "band_container";
 
         imageNode.src = this.band.image_url_large;
         imageNode.className = "band_image";
-        
+
         nameNode.innerText = this.band.name;
         nameNode.className = "band_name";
-     
+
         tourNode.className = "tour_status";
 
         if(this.band.tour_status === true) {
