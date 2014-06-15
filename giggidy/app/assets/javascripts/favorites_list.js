ec731e12ffae8f38f074081a735d811f9918ae57
Zach Karst <zach@karst.co>
2014-06-15 17:39:40 -0400

    Add HTML5 local storage
    Save favorites list in local storage. User can return to website and favorites list will be shown.

--- giggidy/app/assets/javascripts/favorites_list.js
+++ giggidy/app/assets/javascripts/favorites_list.js
@@ -1,15 +1,17 @@
 FavoriteList = function() {
-    this.list = [];
+    this.list = JSON.parse(localStorage.favoriteList) || [];
 };
 
 FavoriteList.prototype = {
     addBand: function(band) {
         this.list.push(band);
+        localStorage.favoriteList = JSON.stringify(this.list);
     },
     removeBand: function(band_literal) {
         this.list.forEach(function(band) {
             if (list[band_literal] === band)
                 list.splice(band_literal, 1);
+        localStorage.favoriteList = JSON.stringify(this.list);
         });
     }
 };
