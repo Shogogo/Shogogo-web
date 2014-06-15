5e5bda573f214a5e5539ac8fdaef53f36b760597
Alexander Pan <alexanderdavidpan@gmail.com>
2014-06-14 21:13:19 -0400

    Add custom post route for signingup/creating users in DB

--- giggidy/config/routes.rb
+++ giggidy/config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+  post "signup" => "user#create", :as => :signup, :format => false
+
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
