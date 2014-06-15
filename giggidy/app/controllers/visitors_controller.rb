c0d2d0abc4ff0a1befadcc9367a748a45b677b00
Alex Pan & Justin Phelps <pair+alexanderdavidpan.justinphelps@devbootcamp.com>
2014-06-15 19:04:45 -0400

    Create get_session method in Visitors Controller - The method will first check to see if the visitor already has a session id - If they do, the method will prepare a json object will session, latitude, longitude, and an array of the visitor's artist preferences by seat geek artist id - Returns the json object

--- giggidy/app/controllers/visitors_controller.rb
+++ giggidy/app/controllers/visitors_controller.rb
@@ -1,4 +1,23 @@
 class VisitorsController < ApplicationController
   def index
+
   end
+
+  def get_session
+    if Visitor::session_exists?(session.id)
+      @session_details =  {sessionId: session.id,lat: User.location("8.8.8.8").location.latitude,lon: User.location("8.8.8.8").location.longitude, preferences: Visitor::check_session(session.id).to_s.gsub(/\"/,"") }
+      @session_details
+      #puts "session: #{@session_details}"
+      #formulate and return a pretty json object with session, [geoip city], [preferences]
+      #for users that have not yet created accounts.
+    end
+  end
+
 end
+
+
+#check if new user
+
+#if returning, get preferenecs, set @user
+
+#on select preferences, update user or create and update.
