abc7afab26e2362e34b8db1fb50137b7dd922c54
Alexander Pan <alexanderdavidpan@gmail.com>
2014-06-14 22:15:34 -0400

    Add create user method in User Controller

--- giggidy/app/controllers/users_controller.rb
+++ giggidy/app/controllers/users_controller.rb
@@ -2,6 +2,14 @@ class UsersController < ApplicationController
   def index
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    end
+  end
+
   private
 
   def user_params
