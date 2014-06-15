b36125ce5c1e0e4dad75ce9309f40bcf5b31b301
Zach Karst <zach@karst.co>
2014-06-15 18:40:42 -0400

    Complete favorite band item remove functionaility
    - User can now click delete button in favorites list to remove selected band
    - Band will be removed from the localstorage favorite list
    - Future implementation will remove from server

---
+++ giggidy/app/assets/javascripts/save_favorites.js
@@ -0,0 +1,11 @@
+SaveFavoriteList = function() {
+    this.list = JSON.parse(localStorage.favoriteList);
+};
+
+FavoriteList.prototype = {
+    save: function(this.list) {
+        
+        
+    }
+
+}
