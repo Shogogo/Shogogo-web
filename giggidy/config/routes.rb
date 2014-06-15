feb44b538d33df896fb46433b84203c42f3cc2f8
Alexander Pan <alexanderdavidpan@gmail.com>
2014-06-15 01:56:17 -0400

    Add custom post route for users#logout

--- giggidy/config/routes.rb
+++ giggidy/config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   post "signup" => "users#create", :as => :signup, :format => false
   post "login" => "users#login", :as => :login, :format => false
-
+  post "logout" => "users#logout", :as => :logout, :format => false
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
