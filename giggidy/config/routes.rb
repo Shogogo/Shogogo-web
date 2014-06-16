89ec8e02429fe7caff29dd721e46c3c4c170b728
Zach Karst <zach@karst.co>
2014-06-15 21:31:57 -0400

    Add interests controller and routes
    - New, create, and destroy only
    - Controller is responsible for rendering form partial to collect user phone number and saving favorites list to database

--- giggidy/config/routes.rb
+++ giggidy/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   root to: 'application#index'
 
+  resources :interests, only: [:new, :create, :destroy]
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
