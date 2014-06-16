b4e7c64d6327ae89f90db03597b6d63e1cac575c
Zach Karst <zach@karst.co>
2014-06-16 15:52:56 -0400

    Remove has_secure_password - not using passwords

--- giggidy/app/models/user.rb
+++ giggidy/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	has_secure_password
+	# has_secure_password
 	# validates :first_name, presence: true
 	# validates :last_name, presence: true
 	# validates :email, presence: true
