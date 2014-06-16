da634019c8206bd4f688d409e485df37ead5ba4f
Alexander Pan <alexanderdavidpan@gmail.com>
2014-06-15 22:45:03 -0400

    Add schema

--- giggidy/db/schema.rb
+++ giggidy/db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20140612230818) do
 
   create_table "users", force: true do |t|
     t.string   "username"
-    t.string   "password_digest"
+    t.string   "password"
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
