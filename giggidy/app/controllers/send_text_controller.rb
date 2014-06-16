12f1456e54d967c8bd5607565eb20d433f0cbdee
Alexander Pan <alexanderdavidpan@gmail.com>
2014-06-16 17:45:25 -0400

    Secret data is stored and accessed in secrets.yml

--- giggidy/app/controllers/send_text_controller.rb
+++ giggidy/app/controllers/send_text_controller.rb
@@ -3,13 +3,13 @@ class SendTextController < ApplicationController
   end
 
   def send_sms(user_phone_number, message)
-    twilio_sid = ENV['twilio_account_sid']
-    twilio_token = ENV['twilio_auth_token']
+  account_sid = TWILIO_ACCOUNT_SID 
+  auth_token = TWILIO_AUTH_TOKEN 
  
-    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+    @twilio_client = Twilio::REST::Client.new account_sid, auth_token
  
     @twilio_client.account.sms.messages.create(
-      :from => ENV['twilio_phone_number'],
+      :from => TWILIO_PHONE_NUMBER,
       :to => user_phone_number,
       :body => message
     )
