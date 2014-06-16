1959fb388d2fa3e5d470956a27c493972d1a35cb
Alexander Pan <alexanderdavidpan@gmail.com>
2014-06-16 14:41:28 -0400

    Create nodes with info about artists' local shows

--- giggidy/app/assets/javascripts/local_shows_presenter.js
+++ giggidy/app/assets/javascripts/local_shows_presenter.js
@@ -1,3 +1,30 @@
 var LocalShowsPresenter = function() {
 };
 
+LocalShowsPresenter.prototype = {
+    present: function(localShows) {
+        var localShowsContainerNode = document.createElement('div');
+
+        localShowsNode.id = "local_shows_container";
+        for (var i=0; i<localShows.length; i++) {
+            var showContainerNode = document.createElement('div');
+            var timeNode = document.createElement('div');
+            var eventNameNode = document.createElement('span');
+            var venueNode = document.createElement('span');
+            var cityNode = document.createElement('span');
+            var stateNode = document.createElement('span');
+            var tixLeftNode = document.createElement('span');
+            var buyTixNode = document.createElement('a');
+
+            timeNode.innerText = localShows[i].event_time;
+            eventNameNode.innerText = localShows[i].event_name;
+            venueNode.innerText = localShows[i].venue;
+            cityNode.innerText = localShows[i].city;
+            stateNode.innerText = localShows[i].state;
+            tixLeftNode.innerText = localShows[i].lowest_price;
+            buyTixNode.innerText; = "BUY NOW" 
+            buyTixNode.attr('href', localShows[i].buy_tix_url); 
+        }
+        return bandPanelNode;
+    }
+};
