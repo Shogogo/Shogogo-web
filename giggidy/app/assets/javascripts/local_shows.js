5caee70d38b65293fb6fb431d51bfef238a2f4ad
Alexander Pan <alexanderdavidpan@gmail.com>
2014-06-16 12:00:09 -0400

    Add preparedLocalShows which prepares query strings for SeatGeek - This method normalizes artist names for SeatGeek API - Grabs user ip - Queries SeatGeek of all artists' events within a default 10 mile radius of the user's ip

--- giggidy/app/assets/javascripts/local_shows.js
+++ giggidy/app/assets/javascripts/local_shows.js
@@ -6,3 +6,12 @@ LocalShows.prototype.getArtistName = function() {
     return artistName;
 };
 
+SearchBox.prototype.preparedLocalShowsQuery = function(artistName) {
+    var words = artistName.replace(/\./g,'').split(' ');
+    var preparedAristName = words.join('-').toLowerCase();
+    var user_ip = $.getJSON("http://smart-ip.net/geoip-json?callback=?", function(data){
+        return data.host;
+    });
+    return 'http://api.seatgeek.com/2/events?geoip='+ user_ip + '&range=10mi&performers.slug=' + preparedAristName;
+};
+
