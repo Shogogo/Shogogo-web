ceafc299e2363b27dc521c24d71fd84a2ee54ac1
Zach Karst <zach@karst.co>
2014-06-16 15:16:17 -0400

    Remove password references user model

--- giggidy/app/models/user.rb
+++ giggidy/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-	has_secure_password
-	validates :username, presence: true
+	# has_secure_password
+	# validates :username, presence: true
 	# validates :first_name, presence: true
 	# validates :last_name, presence: true
 	# validates :email, presence: true
