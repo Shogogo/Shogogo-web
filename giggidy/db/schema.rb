b0bbb9c0b6aeac93246109bef69e9047e4f291dd
Zach Karst <zach@karst.co>
2014-06-16 15:09:27 -0400

    Remove password from user model

--- giggidy/db/schema.rb
+++ giggidy/db/schema.rb
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20140612230818) do
 
   create_table "users", force: true do |t|
     t.string   "username"
-    t.string   "password"
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
