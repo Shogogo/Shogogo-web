77cadd0e9dda93e24005c7fc9af0e180dc7abaa9
Zach Karst <zach@karst.co>
2014-06-17 15:25:06 -0400

    Complete rspec features menu tests
    - User can search for artist
    - Add artist from band container
    - Artist will appear in sidebar
    - User can click "X" button and artist will be removed
    
    All tests passing!

--- giggidy/spec/features/favorites_add_spec.rb
+++ giggidy/spec/features/favorites_add_spec.rb
@@ -1,43 +1,28 @@
-require 'spec_helper'
-
+# require 'spec_helper'
 
 feature "Add artists to favorites" do 
 
-	# before :each, js: true do 
-	# 	visit root_path
-	# 	fill_in "artist-search", with: "Fleetwood"
- #    sleep(2)
- #    page.execute_script %Q{ $('.fbs-item').first().trigger("mouseenter").click(); }
- #    sleep(2)
- #    click_button "Add+"
-	# end
+	before :each, js: true do 
+  	visit root_path
+    fill_in "artist-search", with: "Fleetwood"
+    sleep(1)
+    page.execute_script %Q{ $('.fbs-item').first().trigger("mouseenter").click(); }
+    sleep(1)
+    click_button "Add+"
+    sleep(1)
+  end
 
   scenario "user adds a favorite artist", js: true do
-  	visit root_path
-		fill_in "artist-search", with: "Fleetwood"
-    sleep(2)
-    page.execute_script %Q{ $('.fbs-item').first().trigger("mouseenter").click(); }
-    sleep(2)
-    click_button "Add+"
-    sleep(2)
-  	expect(page).to have_css('#favorites_menu')
-  	expect(page).to have_css('.favorites_band_item')
-  	expect(page).to have_css('.favorites_save')
+    expect(page).to have_content('Notify Me')
+    expect(page).to have_content('Fleetwood Mac')
+    page.execute_script("sessionStorage.clear()")
   end
 
-  scenario "artist snippet contains name" do
-  	expect(page).to have_css(".favorites_band_name")
-	end
-
-
-  scenario "artist snippet contains image" do
-  	expect(page).to have_css('.favorites_band_image')
+  scenario "artist snippet can be deleted", js: true do
+    expect(page).to have_content('Fleetwood Mac')
+    sleep(1)
+    click_button('X')
+    sleep(1)
+  	expect(page).to_not have_content('Fleetwood Mac')
   end
-
-  scenario "artist snippet can be deleted" do
-  	expect(page).to have_css('.favorites_band_remove')
-  	click_button 'X'
-  	expect(page).to_not have_css('.favorites_band_image')
-  end
-
 end
