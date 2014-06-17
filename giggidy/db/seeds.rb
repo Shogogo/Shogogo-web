7ec5672266830edacf0573b9dd64dd072a579ba5
justinphelps <justin.phelps@gmail.com>
2014-06-17 09:40:17 -0400

    remove uneccesary comments from seed file

--- giggidy/db/seeds.rb
+++ giggidy/db/seeds.rb
@@ -3,8 +3,6 @@ User.create(first_name: "zach", last_name: "karst", email: "zach@karst.co", lati
 User.create(first_name: "ryan", last_name: "rebo", email: "ryanpatrickrebo@gmail.com", latitude: 40.7064 , longitude: -74.0094, phone_number: 4064614785)
 User.create(first_name: "alex", last_name: "pan", email: "alexanderdavidpan@gmail.com", latitude: 40.7064 , longitude: -74.0094, phone_number: 8183379919)
 
-40.7064° N, 74.0094° W
-
 [266, 76, 736, 1173, 9052, 1031, 303].each do |seatgeek_id|
   Favorite.create(user_id: 1, seatgeek_artist_id: seatgeek_id)
 end
