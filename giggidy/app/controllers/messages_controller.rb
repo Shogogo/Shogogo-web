97f006707083089d851a21cd084b9cea73093de3
Alexander Pan <alexanderdavidpan@gmail.com>
2014-06-17 11:00:11 -0400

    Fix circular dependency error from ApplicationHelper - Move SMS functions to Messages Controller

--- giggidy/app/controllers/messages_controller.rb
+++ giggidy/app/controllers/messages_controller.rb
@@ -1,17 +1,37 @@
 class MessagesController < ApplicationController
+  include ApplicationHelper
+
   def index
   end
 
   def send_sms(user_phone_number, message)
-  account_sid = TWILIO_ACCOUNT_SID 
-  auth_token = TWILIO_AUTH_TOKEN 
- 
+    account_sid = ENV['twilio_account_sid'] 
+    auth_token = ENV('twilio_auth_token') 
+   
     @twilio_client = Twilio::REST::Client.new account_sid, auth_token
- 
+   
     @twilio_client.account.sms.messages.create(
-      :from => TWILIO_PHONE_NUMBER,
+      :from => 'ShoGoGo',
       :to => user_phone_number,
       :body => message
     )
   end
+
+  def receive_sms
+    process_sms
+  end
+
+  def read_sms
+    account_sid = ENV['twilio_account_sid']
+    auth_token = ENV['twilio_auth_token']
+
+    @client = Twilio::REST::Client.new account_sid, auth_token
+
+    @messages = @client.account.messages.list({:to => ENV['twilio_phone_number'], :date_sent => "2014-17-06"})
+
+    @messages.each do |message|
+      puts message.body
+    end
+  end
+
 end
