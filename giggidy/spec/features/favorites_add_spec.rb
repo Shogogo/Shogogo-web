8f0b248bab4e6bd2d591ea1ad3c3cfc832ac3cff
Ryan Rebo <ryanpatrickrebo@gmail.com>
2014-06-17 12:35:39 -0400

    Add feature testing for favorites -RSPEC feature tests -needs Zach's JS change to 'textContent' to work in Firefox -TODO: run tests after that change is made

--- giggidy/spec/features/favorites_add_spec.rb
+++ giggidy/spec/features/favorites_add_spec.rb
@@ -1,13 +1,43 @@
 require 'spec_helper'
 
+
 feature "Add artists to favorites" do 
 
-  scenario "user adds a favorite artist"
+	# before :each, js: true do 
+	# 	visit root_path
+	# 	fill_in "artist-search", with: "Fleetwood"
+ #    sleep(2)
+ #    page.execute_script %Q{ $('.fbs-item').first().trigger("mouseenter").click(); }
+ #    sleep(2)
+ #    click_button "Add+"
+	# end
 
-  scenario "artist snippet contains name"
+  scenario "user adds a favorite artist", js: true do
+  	visit root_path
+		fill_in "artist-search", with: "Fleetwood"
+    sleep(2)
+    page.execute_script %Q{ $('.fbs-item').first().trigger("mouseenter").click(); }
+    sleep(2)
+    click_button "Add+"
+    sleep(2)
+  	expect(page).to have_css('#favorites_menu')
+  	expect(page).to have_css('.favorites_band_item')
+  	expect(page).to have_css('.favorites_save')
+  end
 
-  scenario "artist snippet contains image"
+  scenario "artist snippet contains name" do
+  	expect(page).to have_css(".favorites_band_name")
+	end
 
-  scenario "artist snippet can be deleted"
+
+  scenario "artist snippet contains image" do
+  	expect(page).to have_css('.favorites_band_image')
+  end
+
+  scenario "artist snippet can be deleted" do
+  	expect(page).to have_css('.favorites_band_remove')
+  	click_button 'X'
+  	expect(page).to_not have_css('.favorites_band_image')
+  end
 
 end
