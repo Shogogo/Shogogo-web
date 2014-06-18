b7f50f50efa9601f18a8b1f3b66a7f36f918e57f
Alexander Pan <alexanderdavidpan@gmail.com>
2014-06-17 21:04:08 -0400

    Add cron job with psuedocode to delete all past events

--- giggidy/config/schedule.rb
+++ giggidy/config/schedule.rb
@@ -6,3 +6,7 @@ every 1.day, :at => "12:00pm" do
   #send out notifications to users about bands' upcoming shows
   rake "events:send_sms"
 end
+
+every 1.day :at => "11:00pm" do
+  #go through all events and delete expired events
+end
