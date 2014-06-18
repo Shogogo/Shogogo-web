8e9fe9b2e68a6b19431789f7e4f955b325a41d05
Ryan Rebo <ryanpatrickrebo@gmail.com>
2014-06-18 17:23:20 -0400

    Add user signin form -field validations to make sure all are filled in before submit -formatting and style -limit on phone number field to 10 digits -password field shows up as 'dots' not letters and integers

--- giggidy/app/assets/javascripts/application_controller.js
+++ giggidy/app/assets/javascripts/application_controller.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function() {
     var bandView = new BandView();
     var localShowsView = new LocalShowsView();
@@ -66,6 +68,7 @@ $(document).ready(function() {
             $('#favorites-menu').html(data);
         }, "html");
     });
+
 
     $( document ).on( "submit", "#phone", function(e) {
         e.preventDefault();
