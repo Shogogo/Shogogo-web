51758e9bc1c71751b391cd8bf8fc5c21d2309a7a
Zach Karst <zach@karst.co>
2014-06-18 05:15:35 -0400

    Edit user unit test - new database architecture

--- giggidy/spec/models/users_model_spec.rb
+++ giggidy/spec/models/users_model_spec.rb
@@ -1,36 +1,25 @@
 require 'spec_helper'
 
 describe User do
+  it { should have_many(:favorites) }
+  
+  it { should have_many(:artists).through(:favorites) }
+  
+  it { should have_many(:events).through(:artists) }
 
-	it "is valid with a phonenumber" do
-	  user = User.new(
-	    phone_number: '4511234567')
-		expect(user).to be_valid 
-	end
-	  
-	it "is invalid without a phonenumber" do
-		user = User.new(phone_number: nil)
-		expect(user).to have(2).errors_on(:phone_number)
-	end
+  it { should validate_presence_of(:name) }
+  
+  it { should validate_presence_of(:phone_number) }
+  
+  it { should validate_uniqueness_of(:phone_number) }
 
-	it "is invalid with a duplicate phonenumber" do 
-		User.create(
-    	phone_number: '5555555555')
-  	user = User.new(
-    	phone_number: '5555555555')
-  	expect(user).to have(1).errors_on(:phone_number)
-	end
+  it { should allow_value('12223334444').for(:phone_number) }
+  
+  it { should_not allow_value('ab223334444').for(:phone_number) }
+  
+  it { should validate_presence_of(:latitude) }
 
-	it "is valid with specific format of phonenumber" do
-		user = User.new(
-			phone_number: '5551233245')
-		expect(user.phone_number).to match /\A\d{10}\z/ 
-	end
+  it { should validate_presence_of(:longitude) }
 
-	it "is not valid if phone is wrong format" do
-		User.new(phone_number: 'A234567').should_not be_valid
-	end
-
+  it { should have_secure_password }
 end
-
-
