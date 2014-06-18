2c77597897191a03b7c864905b5e8e9010840b98
Zach Karst <zach@karst.co>
2014-06-18 12:00:03 -0400

    Add sessions controller
    - Responsible for authenticating user
    - Setting session for visitor and attaching to favorites

---
+++ giggidy/app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_user
+  end
+
+  def session_params
+  end
+end
