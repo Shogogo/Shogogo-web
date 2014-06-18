ef6016139b2d98b1806efa41da62892fc0678f04
justinphelps <justin.phelps@gmail.com>
2014-06-18 15:08:47 -0400

    move map requests to bing

--- giggidy/config/initializers/geocoder.rb
+++ giggidy/config/initializers/geocoder.rb
@@ -1,14 +1,24 @@
-# config/initializers/geocoder.rb
-Geocoder.configure(
+# # config/initializers/geocoder.rb
+# Geocoder.configure(
+#   :bing => { 
+#     :api_key => 'At2fl1rfPvarpTAHir4X66beSAuEhgaJQMnD9gz0EX5_HIUgAIAv80n8QXgJonBi'
+# },
+#   # geocoding service (see below for supported options):
+#   :lookup => :bing,
 
-  # geocoding service (see below for supported options):
-  :lookup => :yandex,
-
-  # IP address geocoding service (see below for supported options):
-  :ip_lookup => :freegeoip,
+#   # IP address geocoding service (see below for supported options):
+#   :ip_lookup => :freegeoip,
 
 
-  # geocoding service request timeout, in seconds (default 3):
-  :timeout => 5
+#   # geocoding service request timeout, in seconds (default 3):
+#   :timeout => 5
 
-)
+# )
+
+
+Geocoder.configure {
+  lookup: :bing,
+  key: 'At2fl1rfPvarpTAHir4X66beSAuEhgaJQMnD9gz0EX5_HIUgAIAv80n8QXgJonBi',
+  cache: Rails.cache,
+
+}
