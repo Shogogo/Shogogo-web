f1ed39f7926c6b9a0f220634f9667ebbb96796c2
justinphelps <justin.phelps@gmail.com>
2014-06-18 12:22:30 -0400

    fix notification type, adjust seed

--- giggidy/app/models/notification.rb
+++ giggidy/app/models/notification.rb
@@ -2,5 +2,5 @@ class Notification < ActiveRecord::Base
   belongs_to :event
   belongs_to :user
 
-  validates :user, :event, :type, :datetime_sent, presence: true
+  validates :user, :event, :notification_type, :datetime_sent, presence: true
 end
