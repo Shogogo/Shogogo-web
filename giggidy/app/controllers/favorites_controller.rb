2bcd1475d92caaf7e7334f412c4ff238a075de98
Zach Karst <zach@karst.co>
2014-06-18 11:55:35 -0400

    Create favorites controller
    - Function as primary controller for one-page app
    - Index, create, destroy methods only

---
+++ giggidy/app/controllers/favorites_controller.rb
@@ -0,0 +1,23 @@
+class FavoritesController < ApplicationController
+  def index
+    get_ip
+  end
+
+  def create
+    @favorite = Favorite.new
+  end
+
+  def destroy
+  end
+
+  private
+
+  def get_user_ip
+    if request.remote_ip == "127.0.0.1"
+      client_ip = "8.8.8.8"
+    else 
+      client_ip = request.remote_ip
+    end
+    @geoinfo = Geocoder.coordinates(client_ip)
+  end
+end
