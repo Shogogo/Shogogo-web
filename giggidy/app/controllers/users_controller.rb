412d0a9ede60306ba38ced4b34d11626d964e9ce
Zach Karst <zach@karst.co>
2014-06-18 12:30:30 -0400

    Remove user authentication from users model.
    - Re-enable protect_from_forgery

--- giggidy/app/controllers/users_controller.rb
+++ giggidy/app/controllers/users_controller.rb
@@ -1,11 +1,5 @@
 class UsersController < ApplicationController
   include ApplicationHelper
-
-  protect_from_forgery except: :create
-
-  def index
-  end
-
   def new
     @user = User.new
     render :partial => 'phone_form'
@@ -35,23 +29,9 @@ class UsersController < ApplicationController
     end
   end
 
-  def login
-    @user = User.find_by_username(user_params[:username])
-    if @user.password_digest = user_params[:password]
-      session[:user_id] = @user.id
-    end
-    redirect_to root_path
-  end
-
-  def logout
-    @user = User.find(session[:user_id])
-    reset_session
-    redirect_to root_path
-  end
-
   private
 
   def user_params
-    params.require(:user).permit(:phone_number, :bands)                          
+    params.require(:user).permit(:phone_number, :bands)
   end
 end
