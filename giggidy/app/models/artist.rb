da12177377e94b5ba3b1b9c76bb6a0e8ebaf53db
Alexander Pan <alexanderdavidpan@gmail.com>
2014-06-18 16:07:52 -0400

    Add method to seed all artist info from SeatGeek

--- giggidy/app/models/artist.rb
+++ giggidy/app/models/artist.rb
@@ -6,4 +6,22 @@ class Artist < ActiveRecord::Base
   accepts_nested_attributes_for :events, allow_destroy: true
 
   validates :name, :seatgeek_id, presence: true, uniqueness: true
+
+  def self.seed_from_seatgeek
+    (1..30000).each do |i|
+      artistInfo = fetch_artist(i)
+      if artistInfo['type'] == "band"
+        Artist.create(name: artistInfo['name'],
+                      seatgeek_id: artistInfo['id'])
+      end
+    end
+  end
+
+  private
+
+  def self.fetch_artist(artist_id)
+    response = Net::HTTP.get(URI.parse("http://api.seatgeek.com/2/performers/#{artist_id}"))
+    JSON.parse(response) 
+  end
+
 end
