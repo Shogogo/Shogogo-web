8afa080901876bc73b2dae7737e7abe8f7617f65
Zach Karst <zach@karst.co>
2014-06-18 05:16:07 -0400

    Add artist unit test full test coverage

---
+++ giggidy/spec/models/artists_model_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe Artist do
+	it { should have_many(:favorites) }
+
+  it { should have_many(:users).through(:favorites) }
+
+  it { should validate_presence_of(:name) }
+  
+  it { should validate_uniqueness_of(:name) }
+  
+  it { should validate_presence_of(:seatgeek_id) }
+  it { should validate_uniqueness_of(:seatgeek_id) }
+end
+
+
