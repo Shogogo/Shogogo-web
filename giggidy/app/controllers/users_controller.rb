1519e5362d0096efb09cdc30e4f6112da98fc1e0
Zach Karst <zach@karst.co>
2014-06-18 19:53:30 -0400

    Remove favorite create from user controller

--- giggidy/app/controllers/users_controller.rb
+++ giggidy/app/controllers/users_controller.rb
@@ -6,28 +6,39 @@ class UsersController < ApplicationController
   end
 
   def create
-    bands = params[:user][:bands]
-    band_array = bands.chomp.split(',').map { |x| x.to_i }
-
-    @user = User.new(phone_number: params[:user][:phone_number])
-    
+    @user = params[:user] ? User.new(params[:user]) : User.new_guest
     if @user.save
-      band_array.each do |band|
-        @user.favorites.build(seatgeek_artist_id: band)
-        @user.save
-      end
+      current_user.move_to(@user) if current_user && current_user.guest?
+      session[:user_id] = @user.id  
 
-      respond_to do |format|
-        format.json { render :json => {:status => 'ok', :message => 'Success!'} }
-        format.js { render :partial => 'verify_phone' }
-      end
-      
-      send_sms(params[:user][:phone_number], "Thank you for using Shogogo!")
-      
     else
-      render :partial => 'shared/errors', :locals => { :object => @user }, :status => :unprocessable_entity
+      render "new"
     end
   end
+
+  # def create
+  #   bands = params[:user][:bands]
+  #   band_array = bands.chomp.split(',').map { |x| x.to_i }
+
+  #   @user = User.new(phone_number: params[:user][:phone_number])
+    
+  #   if @user.save
+  #     band_array.each do |band|
+  #       @user.favorites.build(seatgeek_artist_id: band)
+  #       @user.save
+  #     end
+
+  #     respond_to do |format|
+  #       format.json { render :json => {:status => 'ok', :message => 'Success!'} }
+  #       format.js { render :partial => 'verify_phone' }
+  #     end
+      
+  #     send_sms(params[:user][:phone_number], "Thank you for using Shogogo!")
+      
+  #   else
+  #     render :partial => 'shared/errors', :locals => { :object => @user }, :status => :unprocessable_entity
+  #   end
+  # end
 
   private
 
