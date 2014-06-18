4d4fb46f6eea4f0ac9f348902a738b6fd35ab381
Alexander Pan <alexanderdavidpan@gmail.com>
2014-06-18 12:16:48 -0400

    Move read and reply SMS functions to Messages Controller - Remove them from Application Helper file

--- giggidy/app/helpers/application_helper.rb
+++ giggidy/app/helpers/application_helper.rb
@@ -1,43 +1,16 @@
 module ApplicationHelper
 
   def send_sms(user_phone_number, message)
-    account_sid = 'AC49de56d245eb573da3703df2cfde338a'
-    auth_token = 'c8157d475003fe875f3f3710505c95d9'
+    account_sid = ENV['twilio_account_sid']
+    auth_token = ENV['twilio_auth_token']
    
     @twilio_client = Twilio::REST::Client.new account_sid, auth_token
    
     @twilio_client.account.sms.messages.create(
-      :from => '+19176526862',
+      :from => ENV['twilio_phone_number'],
       :to => user_phone_number,
       :body => message
     )
-  end
-
-  def receive_sms
-    reply = params[:Body].downcase
-    user_phone_number = params[:From]
-
-    user = User.find_by_phone_number(user_phone_number)
-    if reply == "stop"
-      user.destroy
-    elsif reply == "confirm"
-      user.update_atrributes(wants_text: true)
-    end
-
-    redirect_to :root
-  end
-
-  def read_sms
-    account_sid = ENV['twilio_account_sid']
-    auth_token = ENV['twilio_auth_token']
-
-    @client = Twilio::REST::Client.new account_sid, auth_token
-
-    @messages = @client.account.messages.list({:to => ENV['twilio_phone_number'], :date_sent => "2014-17-06"})
-
-    @messages.each do |message|
-      puts message.body
-    end
   end
 
 end
