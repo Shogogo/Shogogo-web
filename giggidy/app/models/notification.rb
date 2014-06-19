62e5527993fb74f258d5a0af1d26f98d4f4fba60
Alexander Pan <alexanderdavidpan@gmail.com>
2014-06-19 11:37:21 -0400

    Remove validations for datetime sent and notification type - No more notification type in Notifications table - Datetime sent isn't necessary because we will just have sent boolean

--- giggidy/app/models/notification.rb
+++ giggidy/app/models/notification.rb
@@ -6,7 +6,7 @@ class Notification < ActiveRecord::Base
   belongs_to :event
   belongs_to :user
 
-  validates :user, :event, :notification_type, :datetime_sent, presence: true
+  validates :user, :event, presence: true
 
   def self.populate_notifications
     users = User.all
@@ -15,7 +15,8 @@ class Notification < ActiveRecord::Base
         users_events = Event.near(user, 100)
         users_events.each do |event|
           if artist.seatgeek_id == event.artist_id
-            Notification.create(user_id: user.id, event_id: event.id)
+            p 'notify'
+            p Notification.create(user_id: user.id, event_id: event.id)
           end
         end
       end
