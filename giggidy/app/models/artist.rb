63bbfe3bdeddf7698c31e054aba051a9fc521b42
Zach Karst <zach@karst.co>
2014-06-18 22:35:13 -0400

    Remove accepts_nested_attributes
    Has_many_through is not properly creating relationships using
    build or shovel methods

--- giggidy/app/models/artist.rb
+++ giggidy/app/models/artist.rb
@@ -3,7 +3,7 @@ class Artist < ActiveRecord::Base
   has_many :users, through: :favorites
   has_many :events
   
-  accepts_nested_attributes_for :events, allow_destroy: true
+  
 
   validates :name, :seatgeek_id, presence: true, uniqueness: true
 
