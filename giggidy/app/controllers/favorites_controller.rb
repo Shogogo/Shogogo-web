58fe39d1bc57d4d248c1e0a15751e6fc101f6ff9
Zach Karst <zach@karst.co>
2014-06-19 10:47:17 -0400

    Add destroy method to favorites_controller.rb
    Current implementation not restful.
    Will refactor to add favorites record to band div in
    favorites menu on create. Then pass record id in params
    on ajax destroy.

--- giggidy/app/controllers/favorites_controller.rb
+++ giggidy/app/controllers/favorites_controller.rb
@@ -3,30 +3,26 @@ class FavoritesController < ApplicationController
     get_user_ip
     @user = User.create(guest: true)
     @user.id
-    session[:guest_id] = @user.id
+    session[:user_id] = @user.id
     @favorites = @user.favorites
   end
 
   def create
-    @user = User.find(session[:guest_id])
+    @user = User.find(session[:user_id])
     @favorite = Favorite.create(user: @user, artist: Artist.where(favorite_params).first_or_create)
-    render :json => { :success => "success", :status_code => "200" }
+    render :json => { :success => "success", :status_code => "200", :id => @favorite.id }
   end
 
   def destroy
+    @user = User.find(session[:user_id])
+    @artist = Artist.where(favorite_params)
+    p @artist.name
+    p @artist.seatgeek_id
+    Favorite.where(user: @user, artist: @artist).destroy
+    render :json => { :success => "success", :status_code => "200" }
   end
 
   private
-
-  def get_user_ip
-    if request.remote_ip == "127.0.0.1"
-      client_ip = "8.8.8.8"
-    else 
-      client_ip = request.remote_ip
-    end
-    @geoinfo = Geocoder.coordinates(client_ip)
-  end
-
   def favorite_params
     params.require(:favorite).permit(:name, :seatgeek_id)
   end
