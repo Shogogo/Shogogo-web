0384318b523dc74ee988b75d11bb4edffe42c403
Alexander Pan <alexanderdavidpan@gmail.com>
2014-06-18 20:08:37 -0400

    Add populate_notifications method with psuedocode

--- giggidy/app/models/notification.rb
+++ giggidy/app/models/notification.rb
@@ -3,4 +3,10 @@ class Notification < ActiveRecord::Base
   belongs_to :user
 
   validates :user, :event, :notification_type, :datetime_sent, presence: true
+
+  def self.populate_notifications
+    #populates notifications based on User.events where (geo radius < 100 miles)
+    #AND user.notiications where(event = event.id).where(notified = false)
+  end
+
 end
