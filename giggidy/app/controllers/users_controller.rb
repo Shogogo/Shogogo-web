06c0e60e73367e259bd34ffe785be5c94e5aaeb8
Zach Karst <zach@karst.co>
2014-06-20 00:29:40 -0400

    Remove duplicate password assignment
    Possilble duplication from merge conflict

--- giggidy/app/controllers/users_controller.rb
+++ giggidy/app/controllers/users_controller.rb
@@ -9,25 +9,15 @@ class UsersController < ApplicationController
     @user.update(phone_number: params[:user][:phone_number], name: params[:user][:name])
     @user.password = params[:user][:password]
     @user.password_confirmation = params[:user][:password]
-    @user.password = params[:user][:password]
-    @user.password_confirmation = params[:user][:password]
     @user.longitude = 40.77 
     @user.latitude = -73.98
     @user.guest = false
     @user.save
     
     # flash[:notice] = "Successfully registered."
-
     send_sms(@user.phone_number, "Thank you for using Shogogo! Reply 'confirm' to verify your number or reply 'stop' to unsubscribe.")
     sleep(10)
     render :json => { :status => 'ok', :message => 'Success!'}
-
-    # if receive a response render success message
-      
-    # render :json => { :success => "success", :status_code => "200" }
-    # else
-    #   render :partial => 'phone_form'
-    # end
   end
 
   private
