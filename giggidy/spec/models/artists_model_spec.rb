ded3302338b3d8fa08c6b0d7cb140ac384c4dc9a
Alexander Pan <alexanderdavidpan@gmail.com>
2014-06-22 13:19:32 -0400

    Add rspec to check that artists have many events

--- giggidy/spec/models/artists_model_spec.rb
+++ giggidy/spec/models/artists_model_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe Artist do
 	it { should have_many(:favorites) }
 
+  it { should have_many(:events) }
+
   it { should have_many(:users).through(:favorites) }
 
   it { should validate_presence_of(:name) }
@@ -10,6 +12,7 @@ describe Artist do
   it { should validate_uniqueness_of(:name) }
   
   it { should validate_presence_of(:seatgeek_id) }
+
   it { should validate_uniqueness_of(:seatgeek_id) }
 end
 
