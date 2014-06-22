4f25dd066a9d3391eb7e4454d38f82f6eca09a6c
Alexander Pan <alexanderdavidpan@gmail.com>
2014-06-22 05:11:31 -0400

    Add search feature test for artist names with commas - Users can search for artists like Tyler, the Creator

--- giggidy/spec/features/artists_search_spec.rb
+++ giggidy/spec/features/artists_search_spec.rb
@@ -21,4 +21,13 @@ feature "Artist search box" do
     page.execute_script %Q{ $('.fbs-item').first().trigger("mouseenter").click(); }
     expect(find_field('search_box').value).to eq "Fleetwood Mac"
   end
+
+  scenario "selects an artist with a comma in the name", js: true do
+    visit root_path
+    fill_in "artist-search", with: "Tyler, the Creator"
+    sleep(1)
+    page.execute_script %Q{ $('.fbs-item').first().trigger("mouseenter").click(); }
+    expect(find_field('search_box').value).to eq "Tyler, the Creator"
+  end
+  
 end
