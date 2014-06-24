09eb9e24543a4008a36ef9ca50d5499544af2c42
Alexander Pan <alexanderdavidpan@gmail.com>
2014-06-23 23:21:42 -0400

    User is now reverse geocoded - Using the user's IP address, we use freegeoip to grab the user's approximate latitude and longitude - Using the approxiate lat and lon, we reverse geocode the user's location

--- giggidy/app/models/user.rb
+++ giggidy/app/models/user.rb
@@ -6,6 +6,10 @@ class User < ActiveRecord::Base
   
   has_secure_password
 
+  reverse_geocoded_by :latitude, :longitude,
+  :address => :location
+  after_validation :reverse_geocode
+
   validates_uniqueness_of :phone_number, :allow_nil => true
   validates_format_of :phone_number, :with => /\A\+1\d{10}\z/, :allow_nil => true, message: "Number must consisits of +1 followed by a 10 digit number, i.e. +15551234567"
 end
