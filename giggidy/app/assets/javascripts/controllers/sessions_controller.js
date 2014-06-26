22b4a6626b282fa5c14f3c50b894e91506201ac1
Zach Karst <zach@karst.co>
2014-06-25 23:49:43 -0400

    Add SessionsController to get login form
    SessionsController is responsible for getting rails partial login
    form and posting user signup and login data

---
+++ giggidy/app/assets/javascripts/controllers/sessions_controller.js
@@ -0,0 +1,20 @@
+var SessionsController = function() {
+    this.sessionsView = new SessionsView();
+    this.favoritesView = new FavoritesView();
+};
+
+SessionsController.prototype = {
+    drawLoginForm: function(data) {
+        this.sessionsView.drawLoginForm(data);
+    },
+
+    signUpListener: function() {
+        _this = this;
+        $( document ).on( "click", this.favoritesView.favoritesSaveButton, function(e) {
+        e.preventDefault();
+        $.get("/users/new", function(data) {
+            _this.drawLoginForm(data);
+        }, "html");
+    });
+    }
+};
