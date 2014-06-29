9277500087c4c26e94683e24ee9f67dee39aa57d
Zach Karst <zach@karst.co>
2014-06-28 22:20:59 -0400

    Add drawUserConfirm method in SessionsView
    - Shows confirmation overlay
    - Hides search box
    - Hides Favorites bar
    - Hides search message

--- giggidy/app/assets/javascripts/views/sessions_view.js
+++ giggidy/app/assets/javascripts/views/sessions_view.js
@@ -1,10 +1,20 @@
 SessionsView = function() {
     this.loginForm = document.getElementsByClassName('local_shows_list')[0];
+    this.overlay = $('.overlay');
     this.favoritesView = new FavoritesView();
+    this.bandView = new BandView();
+    this.searchView = new SearchView();
 };
 
 SessionsView.prototype = {
     drawLoginForm: function(data) {
         $(this.favoritesView.favoritesMenuContainer).html(data);
+    },
+    
+    drawUserConfirm: function() {
+        $(this.overlay).show();
+        $(this.favoritesView.favoritesMenuContainer).hide();
+        $(this.bandView.searchNode).hide();
+        $(this.searchView.searchMessage).hide();
     }
 };
