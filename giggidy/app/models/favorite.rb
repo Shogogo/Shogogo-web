bab2f3d31dcec088706d655df0d546566d75e878
Alexander Pan <alexanderdavidpan@gmail.com>
2014-07-01 10:41:09 -0700

    Remove notify_users class method in Favorite model - notify_users method was not being utilized

--- giggidy/app/models/favorite.rb
+++ giggidy/app/models/favorite.rb
@@ -10,20 +10,16 @@ class Favorite < ActiveRecord::Base
 	def self.fetch_user_artist_ids(user_id)
 		#Will fetch all the users favorite artists event
 		User.find(user_id).favorites.pluck(:seatgeek_artist_id)
-	end		
+	end
 
 	def self.fetch_user_events(artist_ids)
 		#Will fetch all the users favorite artists events
-		artist_ids.each do artist_id 
+		artist_ids.each do artist_id
 			Event.fetch_artist_events(artist_id)
 		end
 	end
 
 	def self.fetch_event(long_lat)
-		JSON.parse(open("http://api.seatgeek.com/2/events?performers.id=#{artist_id}").read)	 
-	end
-
-	def self.notify_users
-    users = User.find(user_ids)
+		JSON.parse(open("http://api.seatgeek.com/2/events?performers.id=#{artist_id}").read)
 	end
 end
