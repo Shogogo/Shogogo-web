86daee45d251daba6a822b413d8c6dfc2e56c5a7
Alexander Pan <alexanderdavidpan@gmail.com>
2014-07-03 20:45:35 -0700

    Fix production config error on load - Make unless statement for config to pull from secrets.yml only if in test/development - If production, the ENV config loop is skipped (because no secrets.yml file on production server) - ENV variables are exported in production

--- giggidy/config/application.rb
+++ giggidy/config/application.rb
@@ -13,10 +13,12 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config = YAML.load(File.read(File.expand_path('../secrets.yml', __FILE__)))
-config.merge! config.fetch(Rails.env, {})
-config.each do |key, value|
-  ENV[key] = value unless value.kind_of? Hash
+unless Rails.env.production?
+  config = YAML.load(File.read(File.expand_path('../secrets.yml', __FILE__)))
+  config.merge! config.fetch(Rails.env, {})
+  config.each do |key, value|
+    ENV[key] = value unless value.kind_of? Hash
+  end
 end
 
 module Giggidy
