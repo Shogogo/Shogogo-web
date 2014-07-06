faacdc960d85ce6067860b9db85a2d487a94308f
Alexander Pan <alexanderdavidpan@gmail.com>
2014-07-05 19:51:03 -0700

    Add test to validate presence of name for confirmed users - When users are no longer guests (after signup) and become confirmed users, the User model will validate the presence of the users' names

--- giggidy/spec/models/users_model_spec.rb
+++ giggidy/spec/models/users_model_spec.rb
@@ -17,20 +17,18 @@ describe User do
     before { subject.stub(:eligible?) { false } }
 
     it { should allow_value(nil).for(:phone_number) }
-
   end
 
   context "if confirmed user" do
     before { subject.stub(:guest?) { false } }
     
-    # it { should_not validate_presence_of(:name) }
+    it { should validate_presence_of(:name) }
   
     # it { should validate_uniqueness_of(:phone_number) }
 
     it { should allow_value('+12223334444').for(:phone_number) }
 
     it { should_not allow_value('2223334567').for(:phone_number) }
-
   end
   
 end
