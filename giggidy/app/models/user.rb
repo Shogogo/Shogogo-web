8c476974ccaa1e234299f2727c51d92e5bc0148a
Alexander Pan <alexanderdavidpan@gmail.com>
2014-07-05 19:21:33 -0700

    Add validation for presence of latitude and longitude - All users, even guests, should have a latitude and longitude

--- giggidy/app/models/user.rb
+++ giggidy/app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
   reverse_geocoded_by :latitude, :longitude
   after_validation :reverse_geocode
 
+  validates :longitude, :latitude, presence: true
   validates_uniqueness_of :phone_number, :allow_nil => true
   validates_format_of :phone_number, :with => /\A\+1\d{10}\z/, :allow_nil => true, message: "Number must consisits of +1 followed by a 10 digit number, i.e. +15551234567"
 end
