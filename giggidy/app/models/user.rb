7cdfd27559333528f8536d19dd79947d3612b465
Alexander Pan <alexanderdavidpan@gmail.com>
2014-07-05 19:49:44 -0700

    Add presence of name validation for confirmed users - When users signup, their guest attribute becomes false so the validator will make sure that the user's name is no longer nil

--- giggidy/app/models/user.rb
+++ giggidy/app/models/user.rb
@@ -10,6 +10,7 @@ class User < ActiveRecord::Base
   after_validation :reverse_geocode
 
   validates :longitude, :latitude, presence: true
+  validates_presence_of :name, :unless => :guest?
   validates_uniqueness_of :phone_number, :allow_nil => true
   validates_format_of :phone_number, :with => /\A\+1\d{10}\z/, :allow_nil => true, message: "Number must consisits of +1 followed by a 10 digit number, i.e. +15551234567"
 end
