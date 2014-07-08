e8e6277173e968d9b76e90a22a80b0c5364d1682
Zach Karst <zach@karst.co>
2014-07-07 23:42:04 -0400

    Refactor js initialization
    - ApplicationController is responsible for adding controllers, defining views, and starting listeners
    - Initialize.js initializes an instance of ApplicationController and calls init()
    - Initialize.js previously contained all jquery listeners

--- giggidy/app/assets/javascripts/controllers/application_controller.js
+++ giggidy/app/assets/javascripts/controllers/application_controller.js
@@ -2,14 +2,26 @@ Shogogo.Controller = function() {
 };
 
 Shogogo.Controller.prototype = {
-    init: function(favoritesController, searchController, sessionsController) {
-        this.favorites = favoritesController;
-        this.search = searchController;
-        this.sessions = sessionsController;
+    init: function() {
+        this._addControllers();
+        this._defineViews();
+        this._startListeners();
     },
 
-    renderLoginView: function() {
-        this.favorites.renderSidebar();
-        this.search.renderSidebarView();
+    _addControllers: function() {
+        this.favorites = new Shogogo.FavoritesController(new Shogogo.FavoritesView());
+        this.search = new Shogogo.SearchController(new SearchBox());
+        this.sessions = new Shogogo.SessionsController();
+    },
+
+    _defineViews: function() {
+        this.sessions.defineView(new Shogogo.SessionsView(domElements));
+        this.search.defineView(new Shogogo.SearchView(domElements));
+    },
+
+    _startListeners: function() {
+        this.sessions.listeners();
+        this.search.listeners();
+        this.favorites.listeners();
     }
 };
--- giggidy/app/assets/javascripts/initialize.js
+++ giggidy/app/assets/javascripts/initialize.js
@@ -1,14 +1,4 @@
 $(document).ready(function() {
     var controller = new Shogogo.Controller();
-    var searchController = new Shogogo.SearchController(new SearchBox());
-    var favoritesController = new Shogogo.FavoritesController(new Shogogo.FavoritesView());
-    var sessionsController = new Shogogo.SessionsController();
-    
-    sessionsController.defineView(new Shogogo.SessionsView(domElements));
-
-    searchController.defineView(new Shogogo.SearchView(domElements));
-    
-    sessionsController.listeners();
-    searchController.listeners();
-    favoritesController.listeners();
+    controller.init();
 });
