1dd1375d2456ab6be8cf03780342c14ca539f138
Zach Karst <zach@karst.co>
2014-07-11 11:57:20 -0400

    Refactor SidebarView render method
    - Remove hard coded css
    - Remove preceding period from css class in options hash

--- giggidy/app/assets/javascripts/views/sidebar_view.js
+++ giggidy/app/assets/javascripts/views/sidebar_view.js
@@ -2,9 +2,9 @@ Shogogo.SidebarView = function(options) {
     this.options = options;
     this.sidebar = options.sidebar;
     this.searchContainer = document.querySelector(options.searchContainer);
-    this.classSearchContainerFavorites = document.querySelector(options.classSearchContainerFavorites);
-    this.classSidebarFavorites = options.classFavorites;
-    this.classSidebarNoFavorites = options.classNoFavorites;
+    this.classSearchContainerFavorites = options.classSearchContainerFavorites;
+    this.classSidebarFavorites = options.classSidebarFavorites;
+    this.classSidebarNoFavorites = options.classSidebarNoFavorites;
     this.classLogin = options.classLogin;
     this.search_message = document.querySelector(options.searchMessage);
 };
@@ -27,13 +27,9 @@ Shogogo.SidebarView.prototype = {
     },
 
     renderSidebar: function() {
-        $('#favorites-menu').removeClass('nofaves').addClass('faves');
-        $('#search_message').hide();
-        $('.search_container').removeClass('search_container').addClass('search_with_faves', { duration:200 });
-        $('.login');
-        // $(this.sidebar).removeClass(this.classSidebarNoFavorites).addClass(this.classSidebarFavorites);
-        // $(this.searchContainer).addClass(this.classSearchContainerFavorites, { duration:300 });
-        // $(this.search_message).hide();
-        // $(this.classLogin).hide();
+        $(this.sidebar).removeClass(this.classSidebarNoFavorites).addClass(this.classSidebarFavorites);
+        $(this.searchContainer).addClass(this.classSearchContainerFavorites, { duration:300 });
+        $(this.search_message).hide();
+        $(this.classLogin).hide();
     }
 };
