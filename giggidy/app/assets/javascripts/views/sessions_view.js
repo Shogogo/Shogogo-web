886d48da65ce916a058b3f487ad3a9e38fcd4677
Zach Karst <zach@karst.co>
2014-07-12 13:02:26 -0400

    Refactor SessionsView.js arguments
    - Remove unused dom elements

--- giggidy/app/assets/javascripts/views/sessions_view.js
+++ giggidy/app/assets/javascripts/views/sessions_view.js
@@ -1,10 +1,7 @@
 Shogogo.SessionsView = function(options) {
     this.options = options;
-    this.signupLink = document.querySelector(options.signupLink);
-    this.sidebar = document.querySelector(options.sidebar);
     this.overlay = document.querySelector(options.overlay);
     this.indexElement = document.querySelector(options.indexElement);
-    this.loginLink = document.querySelector(options.loginLink);
     this.loginForm = options.loginForm;
     this.userForm = options.userForm;
 };
